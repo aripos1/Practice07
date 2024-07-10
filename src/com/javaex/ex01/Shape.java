917eb6fb3b1606cf6bfdb4afe9717c027aaabd7d
aripos1 <ihsjjang@gmail.com>
2024-07-10 17:19:41 +0900

    .

--- src/com/javaex/ex01/Shape.java
+++ src/com/javaex/ex01/Shape.java
@@ -1,16 +1,15 @@
 package com.javaex.ex01;
 
-public  class Shape {
+public class Shape {
 	
 	protected String fillColor;
 	
 	public Shape(String fillColor){
-		this.fillColor=fillColor;
+		this.fillColor = fillColor;
 	}
 	
-	public  void draw() {
+	public void draw() {
 		
 	}
 }
-
 
--- src/com/javaex/ex03/Cat.java
+++ src/com/javaex/ex03/Cat.java
@@ -1,14 +1,14 @@
 package com.javaex.ex03;
 
 public class Cat implements Soundable {
-
+//
 	private String cat;
-
+//
 	public Cat(String cat) {
 		super();
 		this.cat = cat;
 	}
-
+//
 	public String getCat() {
 		return cat;
 	}
@@ -16,7 +16,7 @@ public class Cat implements Soundable {
 	public void setCat(String cat) {
 		this.cat = cat;
 	}
-
+//
 	@Override
 	public String sound() {
 		return cat;
